4a412ce7f2a2e402f69f0b6a31f75beecce6d3ca
Brent Roose <brent.roose@gmail.com>
2026-06-23 17:39:07 +0200

    feat(testing): add model factories (#2182)

---
+++ src/Tempest/Framework/Testing/ModelFactory.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Tempest\Framework\Testing;
+
+use Tempest\Reflection\PropertyReflector;
+
+use function Tempest\Mapper\map;
+use function Tempest\Reflection\reflect;
+use function Tempest\Support\arr;
+
+/** @template TModelClass */
+final class ModelFactory
+{
+    private array $fields = [];
+
+    public function __construct(
+        /** @var class-string<TModelClass> The model class to create an instance of. */
+        private readonly string $modelClass,
+    ) {}
+
+    /**
+     * Make multiple instances of the model class.
+     *
+     * @param int|array $items The number of instances to make, or an array with field values used as a sequence to generate multiple instances.
+     *
+     * @return ModelFactoryCollection<TModelClass>
+     */
+    public function times(int|array $items): ModelFactoryCollection
+    {
+        return new ModelFactoryCollection($this, $items);
+    }
+
+    /**
+     * Set up values that should be used for specific fields when creating a model instance
+     *
+     * @param mixed ...$fields If another instance of a ModelFactory is passed, it will be used to create the value for that property.
+     *
+     * @return self<TModelClass>
+     */
+    public function with(mixed ...$fields): self
+    {
+        return clone($this, [
+            'fields' => [
+                ...$this->fields,
+                ...$fields,
+            ],
+        ]);
+    }
+
+    /**
+     * Make an instance of the model class.
+     *
+     * @return TModelClass
+     */
+    public function make()
+    {
+        $fields = $this->fields;
+
+        foreach ($fields as $key => $value) {
+            if (! $value instanceof ModelFactory) {
+                continue;
+            }
+
+            $fields[$key] = $value->make();
+        }
+
+        $model = map($fields)->to($this->modelClass);
+
+        foreach (reflect($model)->getPublicProperties() as $property) {
+            if ($property->isInitialized($model)) {
+                continue;
+            }
+
+            if ($property->hasDefaultValue()) {
+                continue;
+            }
+
+            if ($property->isNullable()) {
+                $property->setValue($model, null);
+
+                continue;
+            }
+
+            $value = $this->generateValue($property);
+
+            if ($value === null) {
+                continue;
+            }
+
+            $property->setValue($model, $value);
+        }
+
+        return $model;
+    }
+
+    /**
+     * Make an instance of the model class and save it to the database.
+     *
+     * @return TModelClass
+     */
+    public function save()
+    {
+        $model = $this->make();
+
+        $model->save();
+
+        return $model;
+    }
+
+    private function generateValue(PropertyReflector $property): mixed
+    {
+        return match ($property->getType()->getName()) {
+            'string' => arr(['Lorem', 'Ipsum', 'Dolor', 'Sit', 'Amet'])->random(),
+            'int' => random_int(1, 100),
+            'float' => random_int(100, 1000) / 10,
+            'bool' => arr([true, false])->random(),
+            default => null,
+        };
+    }
+}
